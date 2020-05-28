e7f8846a413cc295f4ebb3af7543fd3fd890d5d5
mohammad hassan <mohammadhassanbolivand@gmail.com>
2020-05-28 18:26:40 +0430

    update docs

--- src/Laravel/Facade/Sadad.php
+++ src/Laravel/Facade/Sadad.php
@@ -5,8 +5,8 @@ namespace Sadad\Laravel\Facade;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static request($callbackURL, $Amount, $Description, $Email = null, $Mobile = null, $additionalData = null)
- * @method static verify($amount, $authority)
+ * @method static request($callbackURL, $amount, $dateTime, $orderId, $additionalData = null, $multiplexingData = null)
+ * @method static verify($token)
  * @method static redirect()
  * @method static redirectUrl()
  * @method static getDriver()
